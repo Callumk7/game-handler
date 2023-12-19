46ad7841dc5298121429ca844947592a87d26778
Callum Kloos <callumkloos@gmail.com>
2023-12-19 12:29:04 +0000

    basic game route functionality completed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { bearerAuth } from "hono/bearer-auth";
+import { cors } from "hono/cors";
 
 import playlistsRoute from "./routes/playlists";
 import gamesRoute from "./routes/games";
@@ -8,17 +8,14 @@ type Bindings = {
 	DATABASE_URL: string;
 };
 
-// Bearer token for testing
-const token = "playqIsCool";
-
 const app = new Hono<{ Bindings: Bindings }>();
+app.use("/*", cors());
 app.get("/", (c) => c.text("Hello Hono!"));
 
 // PLAYLISTS
 app.route("/playlists", playlistsRoute);
 
 // GAMES
-app.use("/games", bearerAuth({ token }));
 app.route("/games", gamesRoute);
 
 export default app;
--- src/routes/games.ts
+++ src/routes/games.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { drizzleClient } from "../db";
 import { IGDBGame, IGDBGameSchema } from "../types/games";
 import { GameInsert, games } from "../db/schema/games";
+import { uuidv4 } from "../util/generate-uuid";
 
 type Bindings = {
 	DATABASE_URL: string;
@@ -37,14 +38,52 @@ app.post("/", async (c) => {
 		}
 	});
 
-	// Insert the valid games into the database.
-	const gameInsert: GameInsert[] = validGames.map((game) => ({
-		id: `game_${game.id}`,
-		gameId: game.id,
-		title: game.name,
-	}));
+	// How are we going to handle the presence of invalid games?
+	// We can return an error for the entire request, or process the rest of the valid games,
+	// and return the invalid games in the response.
 
-	const insertedGames = await db.insert(games).values(gameInsert).returning();
+	// Insert the valid games into the database.
+	const gameInsert: GameInsert[] = validGames.map((game) => {
+		let gameInsert: GameInsert = {
+			id: `game_${uuidv4()}`,
+			title: game.name,
+			gameId: game.id,
+		};
+
+		if (game.storyline) {
+			gameInsert.storyline = game.storyline;
+		}
+
+		if (game.follows) {
+			gameInsert.externalFollows = game.follows;
+		}
+
+		if (game.aggregated_rating) {
+			gameInsert.aggregatedRating = Math.floor(game.aggregated_rating);
+		}
+
+		if (game.aggregated_rating_count) {
+			gameInsert.aggregatedRatingCount = game.aggregated_rating_count;
+		}
+
+		if (game.rating) {
+			gameInsert.rating = Math.floor(game.rating);
+		}
+
+		if (game.first_release_date) {
+			gameInsert.firstReleaseDate = new Date(
+				game.first_release_date * 1000,
+			);
+		}
+
+		return gameInsert;
+	});
+
+	const insertedGames = await db
+		.insert(games)
+		.values(gameInsert)
+		.onConflictDoNothing()
+		.returning();
 	return c.json({ insertedGames, invalidGames });
 });
 
